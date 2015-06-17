c173e163b2d58dcd0e428ababd33d178f3467e3f
ryane <ryanesc@gmail.com>
2015-06-17 09:48:20 -0400

    support adding certificates to container

--- zookeeper-3.4/zk_config.sh
+++ zookeeper-3.4/zk_config.sh
@@ -7,6 +7,14 @@
 set -e 
 set -x 
 
+# set up SSL
+if [ "$(ls -A /usr/local/share/ca-certificates)" ]; then
+    # normally we'd use update-ca-certificates, but something about running it in
+    # Alpine is off, and the certs don't get added. Fortunately, we only need to
+    # add ca-certificates to the global store and it's all plain text.
+    cat /usr/local/share/ca-certificates/* >> /etc/ssl/certs/ca-certificates.crt
+fi
+
 #This is the Zookeeper ID for the node.
 # Supply if more than one node in the cluster
 export ZK_ID=${ZK_ID:-1}
